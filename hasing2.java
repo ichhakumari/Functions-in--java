49ef4766b588bd2c22dc000e10d81d0f195d0352
Ichha kumari <143002729+ichhakumari@users.noreply.github.com>
2024-02-09 21:30:50 +0530

    Add files via upload

---
+++ hasing2.java
@@ -0,0 +1,54 @@
+
+// character hashing  for lower case....
+import java.util.Scanner;
+
+public class hasing2 {
+
+    // public static void main(String[] args) {
+    // Scanner sc = new Scanner(System.in);
+
+    // String s;
+    // s = sc.next();
+
+    // // precompute:
+    // // int[] hash = new int[26];
+    // // for (int i = 0; i < s.length(); i++) {
+    // // hash[s.charAt(i) - 'a']++;
+    // // }
+
+    // // int q;
+    // // q = sc.nextInt();
+    // // while (q-- > 0) {
+    // // char c;
+    // // c = sc.next().charAt(0);
+    // // // fetch:
+    // // System.out.println(hash[c - 'a']);
+    // // }
+
+    // }
+
+    // }
+
+    // for upper case & lowercase both...
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String s;
+        s = sc.next();
+
+        // precompute:
+        int[] hash = new int[256];
+        for (int i = 0; i < s.length(); i++) {
+            hash[s.charAt(i)]++;
+        }
+
+        int q;
+        q = sc.nextInt();
+        while (q-- > 0) {
+            char c;
+            c = sc.next().charAt(0);
+            // fetch:
+            System.out.println(hash[c]);
+        }
+    }
+}
